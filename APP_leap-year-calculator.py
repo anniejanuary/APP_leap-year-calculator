a326781fe82130a66c6393f354cb1f70fc2b8150
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-22 13:35:54 +0200

    Create APP_leap-year-calculator.py

---
+++ APP_leap-year-calculator.py
@@ -0,0 +1,18 @@
+#it's a leap year:
+# on every year that is evenly divisible by 4 
+# **except** every year that is evenly divisible by 100 
+# **unless** the year is also evenly divisible by 400
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0: 
+    if year % 100 != 0:
+        print("Leap year")
+    if year % 100 == 0 and year % 400 != 0:
+        print("Not leap year")
+    elif year % 100 == 0 and year % 400 == 0:
+        print("Leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
