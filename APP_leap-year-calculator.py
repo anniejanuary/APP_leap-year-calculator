11b37fff43c5cd03d1e6c36a858c78a28370645b
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-22 13:46:45 +0200

    Update APP_leap-year-calculator.py

--- APP_leap-year-calculator.py
+++ APP_leap-year-calculator.py
@@ -5,6 +5,23 @@
 
 year = int(input("Which year do you want to check? "))
 
+# more nested:
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
+
+
+
+# less nested :
+
 if year % 4 == 0: 
     if year % 100 != 0:
         print("Leap year")
